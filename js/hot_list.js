0c0b63bda067ced47cf16ca8a29eb6d3983bfcd0
guoshuang <1460840442@qq.com>
2018-01-20 17:49:47 +0800

    2018.1.20
    修改统一基本样式

--- js/hot_list.js
+++ js/hot_list.js
@@ -234,19 +234,7 @@
   var imgArry=['img/tg_select.png','img/rm_select.png','img/merchant_icon_select.png','img/user_select.png','img/more_select.png'];
   var imgAy=['img/tg_normal.png','img/rm_normal.png','img/merchant_icon_normal.png','img/user_normal.png','img/more_normal.png']
 
-  for (var i=0;i<naver.length;i++) {
-    naver[i].index=i;
-    naver[i].onclick=function () {
-    this.style.borderBottom='solid 2px rgb(254,148,148)';
-    navImg[this.index].src=imgArry[this.index];
-    
-    }
-    
-    naver[i].onmouseout=function () {
-    this.style.borderBottom='';
-    navImg[this.index].src=imgAy[this.index];
-    }
-  }
+  
 $(function (){
   $('#share').click(function (){
     $('.baidu_share').show();
